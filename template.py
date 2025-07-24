173420e4df4fde652e1075c89af7fd21ec6b69d7
audrio <zerotwo8802@gmail.com>
2025-07-24 18:13:33 +0700

    folder structure revision

--- template.py
+++ template.py
@@ -2,26 +2,26 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO, format='[%(assctime)s]: %(message)s:')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 project_name = "cnnClassifier"
 
 list_of_files = [
-    "Chicken-Disease-Classification/.github/workflows/.gitkeep",
-    f"Chicken-Disease-Classification/src/{project_name}/__init__.py",
-    f"Chicken-Disease-Classification/src/{project_name}/components/__init__.py",
-    f"Chicken-Disease-Classification/src/{project_name}/utils/__init__.py",
-    f"Chicken-Disease-Classification/src/{project_name}/config/__init__.py",
-    f"Chicken-Disease-Classification/src/{project_name}/config/configuration.py",
-    f"Chicken-Disease-Classification/src/{project_name}/pipeline/__init__.py",
-    f"Chicken-Disease-Classification/src/{project_name}/entity/__innit__.py",
-    f"Chicken-Disease-Classification/src/{project_name}/constants/__init__.py",
-    "Chicken-Disease-Classification/config/config.yaml",
-    "Chicken-Disease-Classification/dvc.yaml",
-    "Chicken-Disease-Classification/params.yaml",
-    "Chicken-Disease-Classification/requirements.txt",
-    "Chicken-Disease-Classification/setup.py",
-    "Chicken-Disease-Classification/research/trials.ipynb"
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb"
 ]
 
 for filepath in list_of_files:
